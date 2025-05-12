a205fb06a06d5b44cf3003f3c8ad3305116ca03f
Juan Cruz Sabio <sabiojuancruz2@gmail.com>
2025-05-12 10:44:40 -0300

    crear objeto random

---
+++ Assets/Instantiator/Instantiator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int min;
+    public int max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clonar(GameObject objeto)
+    {
+        Instantiate(objeto);
+    }
+}
---
+++ Assets/Instantiator/RandomClone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomClone : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public GameObject cubePrefab;
+    public GameObject cylinderPrefab;
+    GameObject clone;
+    public int min;
+    public int max;
+    int randomNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void randomClone()
+    {
+        randomNumber = Random.Range(min, max);
+
+        switch (randomNumber)
+        {
+            case 1:
+                clone = spherePrefab;
+                break;
+
+            case 2:
+                clone = cubePrefab;
+                break;
+
+            case 3:
+                clone = cylinderPrefab;
+                break;
+        }
+
+        Instantiate(clone);
+    }
+}
