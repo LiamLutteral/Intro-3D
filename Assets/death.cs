a6b78dbd223f3fb356e51e4d75d36e7e09759407
LiamLutteral <49008662@est.ort.edu.ar>
2025-05-15 10:39:12 -0300

    x

--- Assets/death.cs
+++ Assets/death.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class death : MonoBehaviour
+    
 {
+    public float Death = -20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class death : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < Death)
+        {
+            Debug.Log("respawn");
+            SceneManager.LoadScene("FPS Parkur");
+        }
+
     }
 }
