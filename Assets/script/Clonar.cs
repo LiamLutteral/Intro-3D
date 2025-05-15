86492405d65026207e902737e8eb9d79bd873229
LiamLutteral <49008662@est.ort.edu.ar>
2025-05-15 10:31:58 -0300

    x

---
+++ Assets/script/Clonar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clonar : MonoBehaviour
+{
+    public GameObject cuboprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ClonarCubo()
+    {
+        Instantiate(cuboprefab);
+    }
+}
