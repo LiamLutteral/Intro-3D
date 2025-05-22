cb5b2df9eebe1ed11228458571eb2e7dcdf69bc4
LiamLutteral <49008662@est.ort.edu.ar>
2025-05-22 10:43:15 -0300

    x

---
+++ Assets/colisiones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log('a');
+        if (col.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+        }
+       
+    }
+}
