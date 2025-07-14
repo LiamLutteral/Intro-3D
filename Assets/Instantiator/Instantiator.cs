4820cdfd608d6d6cb068ab03567023409671557c
LiamLutteral <49008662@est.ort.edu.ar>
2025-07-14 09:16:16 -0300

    x

--- Assets/Instantiator/Instantiator.cs
+++ Assets/Instantiator/Instantiator.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Instantiator : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public int min;
-    public int max;
+    public GameObject barrilPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +14,22 @@ public class Instantiator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void clonar(GameObject objeto)
     {
-        Instantiate(objeto);
+        // Aquí puedes agregar cualquier lógica que necesites para la clonación si quieres
+    }
+
+    public void clonarBarril()
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            // Usar transform.position para obtener la posición
+            Vector3 position = new Vector3(-2.5f, 17.31f, 36.2f) + Random.insideUnitSphere * 100f;
+
+            // Instanciar el prefab en la misma posición con rotación cero (sin rotación)
+            Instantiate(barrilPrefab, position, Quaternion.identity);
+        }
     }
 }
---
+++ Assets/interactionArea.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionArea : MonoBehaviour
+{
+    public score scoreManager;
+    public GameObject UIInteractionMessage;
+    GameObject currentInteractable = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = FindObjectOfType<score>();
+        UIInteractionMessage.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        mercaderia mercaderia = other.GetComponentInParent<mercaderia>();
+        
+
+        if (mercaderia)
+        {
+            currentInteractable = other.gameObject;
+            UIInteractionMessage.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        mercaderia mercaderia = other.GetComponentInParent<mercaderia>();
+
+        if (mercaderia)
+        {
+            UIInteractionMessage.SetActive(false);
+            if (currentInteractable == other.gameObject) currentInteractable = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            mercaderia mercaderia = currentInteractable.GetComponentInParent<mercaderia>();
+            Instantiator instantiator = currentInteractable.GetComponentInParent<Instantiator>();
+
+            if (instantiator)
+            {
+                instantiator.clonarBarril();
+            }
+
+            scoreManager.AddScore(mercaderia.scorePoints);
+            Destroy(currentInteractable);
+            currentInteractable = null;
+            UIInteractionMessage.SetActive(false);
+        }
+    }
+}
